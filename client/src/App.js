4dc49d9ead47131912c3e5d90df3dbc631f78242
Pooja Patel <poojapatel.web@gmail.com>
2021-09-24 19:13:43 -0400

    fix the datahelpers module not found

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import useApplicationData from './hook/useApplicationData';
+
 
 const App = () => {
   const {
       state,
       dispatch
   } = useApplicationData();
+  console.log("state",state);
     const userList = state.users.map((user) => (<li key={user.id} > {user.first_name} {user.last_name} {user.email} </li>
 ));
 return (<div className="App" >
