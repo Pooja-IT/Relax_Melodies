7e7768ee1aa8771ae27686bb27b1f1fe5e7d6ba3
Jalen Duggan <jalen.j.duggan@gmail.com>
2021-09-27 14:49:59 -0400

    added component with hard code data for styling

--- client/src/components/Application.js
+++ client/src/components/Application.js
@@ -1,21 +1,23 @@
-import './Application.css.css';
+// import './Application.css.css';
 import useApplicationData from '../hook/useApplicationData';
-
+import YogaSession from './YogaSession';
 
 const App = () => {
-  const {
-      state,
-      dispatch
-  } = useApplicationData();
-  console.log("state",state);
-    const userList = state.users.map((user) => (<li key={user.id} > {user.first_name} {user.last_name} {user.email} </li>
-));
-return (<div className="App" >
-  <h1> Users </h1>
+//   const {
+//       state,
+//       dispatch
+//   } = useApplicationData();
+//   console.log("state",state);
+//     const userList = state.users.map((user) => (<li key={user.id} > {user.first_name} {user.last_name} {user.email} </li>
+// ));
+  return (
+    // <div className="App" >
+    //   <h1> Users </h1>
 
-  <ul> {userList} </ul>
-</div >
-);
+    //   <ul> {userList} </ul>
+    // </div >
+    <YogaSession></YogaSession>
+  );
 };
 
 
