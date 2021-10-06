f517d0f968d0d404ac29789d7615517c69154513
Ruchika Gosain <sharma.ruchika34@gmail.com>
2021-10-06 16:30:39 -0400

    Add the styling in userbooking and footer component

--- client/src/components/UserBooking.js
+++ client/src/components/UserBooking.js
@@ -77,7 +77,7 @@ export default function UserBooking() {
         </h5>
       ))}
   
-    <img src="https://slidebazaar.com/wp-content/uploads/2020/07/thank-you-powerpoint-template-1.jpg" className="thankyou" alt="ThankYou"/>
+    <img src="/images/thank.jpeg" className="thankyou" alt="ThankYou"/>
 
     </div>
   )
